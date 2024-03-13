53a76e5644cf6aa1678831352687f6348fba7369
chikien111 <38467294+chikien111@users.noreply.github.com>
2024-03-13 11:19:43 +0700

    update tagline

--- scripts/main.js
+++ scripts/main.js
@@ -149,20 +149,20 @@ App.Site = function () {
       ;
   }
   var fancybox = function () {
-    $("#md-wheel-1").fancybox().trigger('click');
-    $("#md-wheel-2").fancybox().trigger('click');
-    $("#md-wheel-3").fancybox().trigger('click');
-    $("#md-wheel-4").fancybox().trigger('click');
-    $("#md-wheel-5").fancybox().trigger('click');
-    $("#md-wheel-6").fancybox().trigger('click');
-    $("#md-wheel-7").fancybox().trigger('click');
-    $("#md-wheel-8").fancybox().trigger('click');
-    $("#md-wheel-9").fancybox().trigger('click');
-    $("#md-wheel-10").fancybox().trigger('click');
-    $("#md-wheel-11").fancybox().trigger('click');
-    $("#md-wheel-12").fancybox().trigger('click');
-    $("#md-wheel-13").fancybox().trigger('click');
-    $("#md-wheel-14").fancybox().trigger('click');
+    // $("#md-wheel-1").fancybox().trigger('click');
+    // $("#md-wheel-2").fancybox().trigger('click');
+    // $("#md-wheel-3").fancybox().trigger('click');
+    // $("#md-wheel-4").fancybox().trigger('click');
+    // $("#md-wheel-5").fancybox().trigger('click');
+    // $("#md-wheel-6").fancybox().trigger('click');
+    // $("#md-wheel-7").fancybox().trigger('click');
+    // $("#md-wheel-8").fancybox().trigger('click');
+    // $("#md-wheel-9").fancybox().trigger('click');
+    // $("#md-wheel-10").fancybox().trigger('click');
+    // $("#md-wheel-11").fancybox().trigger('click');
+    // $("#md-wheel-12").fancybox().trigger('click');
+    // $("#md-wheel-13").fancybox().trigger('click');
+    // $("#md-wheel-14").fancybox().trigger('click');
   }
 
 
